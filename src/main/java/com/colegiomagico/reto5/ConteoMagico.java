00bdf83da1f0860aa250e866eb18412c8ef35367
Capigamer888 <junior.capi2010@gmail.com>
2026-09-05 11:48:04 -0500

    fix: Corregir el límite en la llamada al método contar en main

--- src/main/java/com/colegiomagico/reto5/ConteoMagico.java
+++ src/main/java/com/colegiomagico/reto5/ConteoMagico.java
@@ -15,6 +15,6 @@ public class ConteoMagico {
         return suma;
     }
     public static void main(String[] args) {
-        System.out.println("Conteo hasta 4: " + contar(5));
+        System.out.println("Conteo hasta 4: " + contar(4));
     }
 }
