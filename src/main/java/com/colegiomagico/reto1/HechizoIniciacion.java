4bee8449dd771491802192aa2dda5048d4493ae6
Capigamer888 <junior.capi2010@gmail.com>
2026-09-05 11:03:50 -0500

    feat: Se impleto los datos necesarios para cumplir con la funcion invocar

--- src/main/java/com/colegiomagico/reto1/HechizoIniciacion.java
+++ src/main/java/com/colegiomagico/reto1/HechizoIniciacion.java
@@ -6,7 +6,7 @@ package com.colegiomagico.reto1;
 public class HechizoIniciacion {
 
     public static String invocar() {
-        return ""; // TODO: Cambia esto
+        return "Mi nombre es Petpito Perez y el profesor se llama Juan Jose"; // TODO: Cambia esto
     }
 
     public static void main(String[] args) {
