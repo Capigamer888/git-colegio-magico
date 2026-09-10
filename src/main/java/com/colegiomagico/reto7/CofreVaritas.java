b6da56ca4efb733fb7160ed13035c005143b18b9
Capigamer888 <junior.capi2010@gmail.com>
2026-09-10 02:23:29 +0000

    feat: Implementar metodo para conseguir el numero mas grande del array tamanos

--- src/main/java/com/colegiomagico/reto7/CofreVaritas.java
+++ src/main/java/com/colegiomagico/reto7/CofreVaritas.java
@@ -6,7 +6,7 @@ package com.colegiomagico.reto7;
 public class CofreVaritas {
 
     public static int varitaMasPoderosa(int[] tamanos) {
-        return 0; // TODO: Cambia esto
+        return java.util.Arrays.stream(tamanos).max().getAsInt();
     }
 
     public static void main(String[] args) {
