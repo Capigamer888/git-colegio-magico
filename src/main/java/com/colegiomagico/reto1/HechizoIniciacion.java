035ec4e1fedae9d9e1c9d83f659c06ab9e4ee3e6
Capigamer888 <junior.capi2010@gmail.com>
2026-09-10 02:04:00 +0000

    fix: Corregir errores de ortografía

--- src/main/java/com/colegiomagico/reto1/HechizoIniciacion.java
+++ src/main/java/com/colegiomagico/reto1/HechizoIniciacion.java
@@ -6,7 +6,7 @@ package com.colegiomagico.reto1;
 public class HechizoIniciacion {
 
     public static String invocar() {
-        return "Mi nombre es Petpito Perez y el profesor se llama Juan Jose"; 
+        return "Mi nombre es Pepito Pérez y el profesor se llama Juan Jose"; 
     }
 
     public static void main(String[] args) {
